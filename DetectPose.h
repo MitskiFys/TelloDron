bd199d07063637a9ebae0d6be21f72152d242b4a
Alexander Novikov <mitskifys@gmail.com>
2022-04-23 02:42:22 +0300

    sort track

---
+++ DetectPose.h
@@ -0,0 +1,10 @@
+#pragma once
+
+
+class DetectPose
+{
+public:
+
+private:
+
+};
--- main.cpp
+++ main.cpp
@@ -9,9 +9,12 @@
 #include "videoOutput.h"
 #include "detectNet.h"
 #include "poseNet.h"
+#include <set>
 
 #include "jetson-utils/cudaFont.h"
 #include "jetson-utils/cudaDraw.h"
+#include "include/sort-cpp/KalmanTracker.h"
+#include "include/sort-cpp/Hungarian.h"
 #include <stdio.h>
 #include <inttypes.h>
 
@@ -173,6 +176,24 @@ bool isElbowUp(const double shoulderAngle, const double elbowAngle)
 	return false;
 }
 
+typedef struct TrackingBox
+{
+	int id;
+	Rect_<float> box;
+}TrackingBox;
+
+// Computes IOU between two bounding boxes
+double GetIOU(Rect_<float> bb_test, Rect_<float> bb_gt)
+{
+	float in = (bb_test & bb_gt).area();
+	float un = bb_test.area() + bb_gt.area() - in;
+
+	if (un < std::numeric_limits<double>::epsilon())
+		return 0;
+
+	return (double)(in / un);
+}
+
 int main()
 {
 	DronControl dron;
@@ -222,6 +243,24 @@ int main()
 		return 0;
 	}
 
+
+	vector<TrackingBox> detData;
+	vector<KalmanTracker> trackers;
+	vector<Rect_<float>> predictedBoxes;
+	vector<vector<double>> iouMatrix;
+	vector<int> assignment;
+	set<int> unmatchedDetections;
+	vector<cv::Point> matchedPairs;
+	vector<TrackingBox> frameTrackingResult;
+	set<int> unmatchedTrajectories;
+	unsigned int trkNum = 0;
+	unsigned int detNum = 0;
+	set<int> allItems;
+	set<int> matchedItems;
+	int max_age = 1;
+	int min_hits = 3;
+	double iouThreshold = 0.3;
+
 	while( !signal_recieved )
 	{
 		// capture next image image
@@ -248,32 +287,156 @@ int main()
 			continue;
 		}
 
+		bool isFirstFrame = true;
+
 		// render outputs
 		if( output != NULL )
 		{
-			if( font != NULL)
+			for (const auto& pose : poses)
 			{
-				for (const auto& pose : poses)
-				{
-					std::string str = std::to_string((unsigned int)pose.ID).append(" - person");
-					font->OverlayText(image, input->GetWidth(), input->GetHeight(),
-									  str.c_str(), pose.Left, pose.Top, make_float4(255, 255, 255, 255), make_float4(0, 0, 0, 100));
-
-					drawTrackingStrategy(net, pose, image, input);
-					//std::cout << getAngleBtwThreePoints(net, pose, LEFT_SHOULDER, LEFT_ELBOW, LEFT_WRIST) << std::endl;
-					std::cout << isArmUp(getAngleBtwThreePoints(net, pose, NECK, LEFT_SHOULDER, LEFT_ELBOW),getAngleBtwThreePoints(net, pose, LEFT_SHOULDER, LEFT_ELBOW, LEFT_WRIST)) << std::endl;
-					std::cout << isArmUp(getAngleBtwThreePoints(net, pose, NECK, RIGHT_SHOULDER, RIGHT_ELBOW) * -1,getAngleBtwThreePoints(net, pose, RIGHT_SHOULDER, RIGHT_ELBOW, RIGHT_WRIST)) << std::endl;
-					setTrackCorrection(net, input, pose, dron);
-					//const auto distanceToCentre = sqrt(pow((neckPoint.x - input->GetWidth() / 2), 2) + pow((neckPoint.y - input->GetHeight() / 2), 2));
-					//getAngleBtwThreePoints(net, pose, LEFT_SHOULDER, LEFT_ELBOW, LEFT_WRIST);
-					//getAngleBtwThreePoints(net, pose, NECK, LEFT_SHOULDER, LEFT_ELBOW);
-//					for (const auto& point : pose.Keypoints)
-//					{
-//						std::cout << net->GetKeypointName(point.ID) << std::endl;
-//					}
-				}
-
+				TrackingBox tb;
+				tb.id = -1;
+				tb.box = Rect_<float>(Point_<float>(pose.Left, pose.Right), Point_<float>(pose.Top, pose.Bottom));
+				detData.push_back(tb);
 			}
+
+			if (trackers.size() == 0 && isFirstFrame) // the first frame met
+			{
+				isFirstFrame = false;
+				// initialize kalman trackers using first detections.
+				for (unsigned int i = 0; i < detData.size(); i++)
+				{
+					KalmanTracker trk = KalmanTracker(detData[i].box);
+					trackers.push_back(trk);
+				}
+				// output the first frame detections
+				for (unsigned int id = 0; id < detData.size(); id++)
+				{
+					TrackingBox tb = detData[id];
+					std::cout << id + 1 << "," << tb.box.x << "," << tb.box.y << "," << tb.box.width + tb.box.x << "," << tb.box.height + tb.box.y << ",1,-1,-1,-1" << std::endl;
+				}
+			}
+			// 3.1. get predicted locations from existing trackers.
+			predictedBoxes.clear();
+
+			for (auto it = trackers.begin(); it != trackers.end();)
+			{
+				Rect_<float> pBox = (*it).predict();
+				if (pBox.x >= 0 && pBox.y >= 0)
+				{
+					predictedBoxes.push_back(pBox);
+					it++;
+				}
+				else
+				{
+					it = trackers.erase(it);
+					//cerr << "Box invalid at frame: " << frame_count << endl;
+				}
+			}
+
+			// 3.2. associate detections to tracked object (both represented as bounding boxes)
+			// dets : detFrameData[fi]
+			trkNum = predictedBoxes.size();
+			detNum = detData.size();
+
+			iouMatrix.clear();
+			iouMatrix.resize(trkNum, vector<double>(detNum, 0));
+			for (unsigned int i = 0; i < trkNum; i++) // compute iou matrix as a distance matrix
+			{
+				for (unsigned int j = 0; j < detNum; j++)
+				{
+					// use 1-iou because the hungarian algorithm computes a minimum-cost assignment.
+					iouMatrix[i][j] = 1 - GetIOU(predictedBoxes[i], detData[j].box);
+				}
+			}
+
+			// solve the assignment problem using hungarian algorithm.
+			// the resulting assignment is [track(prediction) : detection], with len=preNum
+			HungarianAlgorithm HungAlgo;
+			assignment.clear();
+			HungAlgo.Solve(iouMatrix, assignment);
+			// find matches, unmatched_detections and unmatched_predictions
+			unmatchedTrajectories.clear();
+			unmatchedDetections.clear();
+			allItems.clear();
+			matchedItems.clear();
+
+			if (detNum > trkNum) //	there are unmatched detections
+			{
+				for (unsigned int n = 0; n < detNum; n++)
+					allItems.insert(n);
+
+				for (unsigned int i = 0; i < trkNum; ++i)
+					matchedItems.insert(assignment[i]);
+
+				set_difference(allItems.begin(), allItems.end(),
+					matchedItems.begin(), matchedItems.end(),
+					insert_iterator<set<int>>(unmatchedDetections, unmatchedDetections.begin()));
+			}
+			else if (detNum < trkNum) // there are unmatched trajectory/predictions
+			{
+				for (unsigned int i = 0; i < trkNum; ++i)
+					if (assignment[i] == -1) // unassigned label will be set as -1 in the assignment algorithm
+						unmatchedTrajectories.insert(i);
+			}
+			else
+				;
+
+			// filter out matched with low IOU
+			matchedPairs.clear();
+			for (unsigned int i = 0; i < trkNum; ++i)
+			{
+				if (assignment[i] == -1) // pass over invalid values
+					continue;
+				if (1 - iouMatrix[i][assignment[i]] < iouThreshold)
+				{
+					unmatchedTrajectories.insert(i);
+					unmatchedDetections.insert(assignment[i]);
+				}
+				else
+					matchedPairs.push_back(cv::Point(i, assignment[i]));
+			}
+			// 3.3. updating trackers
+
+			// update matched trackers with assigned detections.
+			// each prediction is corresponding to a tracker
+			int detIdx, trkIdx;
+			for (unsigned int i = 0; i < matchedPairs.size(); i++)
+			{
+				trkIdx = matchedPairs[i].x;
+				detIdx = matchedPairs[i].y;
+				trackers[trkIdx].update(detData[detIdx].box);
+			}
+
+			// create and initialise new trackers for unmatched detections
+			for (auto umd : unmatchedDetections)
+			{
+				KalmanTracker tracker = KalmanTracker(detData[umd].box);
+				trackers.push_back(tracker);
+			}
+			// get trackers' output
+			frameTrackingResult.clear();
+			for (auto it = trackers.begin(); it != trackers.end();)
+			{
+				if (((*it).m_time_since_update < 1) &&
+					((*it).m_hit_streak >= min_hits))
+				{
+					TrackingBox res;
+					res.box = (*it).get_state();
+					res.id = (*it).m_id + 1;
+					frameTrackingResult.push_back(res);
+					it++;
+				}
+				else
+				it++;
+
+				// remove dead tracklet
+				if (it != trackers.end() && (*it).m_time_since_update > max_age)
+					it = trackers.erase(it);
+			}
+			for (auto tb : frameTrackingResult)
+				std::cout << "," << tb.id << "," << tb.box.x << "," << tb.box.y << "," << tb.box.width << "," << tb.box.height << ",1,-1,-1,-1" << std::endl;
+			detData.clear();
 
 			output->Render(image, input->GetWidth(), input->GetHeight());
 
@@ -291,5 +454,10 @@ int main()
 		//net->PrintProfilerTimes();
 
 	}
+	//					std::string str = std::to_string((unsigned int)pose.ID).append(" - person");
+	//					font->OverlayText(image, input->GetWidth(), input->GetHeight(),
+	//									  str.c_str(), pose.Left, pose.Top, make_float4(255, 255, 255, 255), make_float4(0, 0, 0, 100));
+	//					drawTrackingStrategy(net, pose, image, input);
+	//					setTrackCorrection(net, input, pose, dron);
 	return 0;
 }
